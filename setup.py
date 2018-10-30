c3f0ecabc1adb03569d6c72797ef5718943e6a92
Kris Dorosz <cypreess@gmail.com>
2018-10-30 18:19:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     license='MIT',
     author='Kris Dorosz',
     author_email='cypreess@gmail.com',
-    description='Minimalistic package manager for snippets.',
+    description='Track snippets use in your codebase with ease',
     install_requires=['requests>=2.18', 'termcolor>=1.1.0', 'PyYAML>=3.13'],
     scripts=['bin/snipty']
 )
