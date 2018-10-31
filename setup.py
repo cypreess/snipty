f0af1429e4b5b2ddd8499784cce30109feefd2e2
Kris Dorosz <cypreess@gmail.com>
2018-10-31 15:26:02 +0100

    setup py long description

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 from snipty import __VERSION__
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='snipty',
     version=__VERSION__,
@@ -11,6 +14,13 @@ setup(
     author='Kris Dorosz',
     author_email='cypreess@gmail.com',
     description='Track snippets use in your codebase with ease',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=['requests>=2.18', 'termcolor>=1.1.0', 'PyYAML>=3.13'],
-    scripts=['bin/snipty']
+    scripts=['bin/snipty'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
